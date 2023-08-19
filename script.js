335f08b2bcf4751f4c62fd6058f5c3dff8364110
Sathwik Sadashay V <sathwiksv30@gmail.com>
2023-08-19 10:42:29 +0530

    first commit-problem1

---
+++ script.js
@@ -0,0 +1,16 @@
+const array = [1, 8, -9, 3, 5, -4, 0, 1, 10, 10, 10, -5, 5, 5];
+
+
+function positiveSum(a) {
+    var result = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > 0)
+            result += a[i];
+        else
+            continue;
+    }
+    return result;
+}
+
+console.log(positiveSum(array))
+
