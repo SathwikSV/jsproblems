c94d2c71b81c9609bb53934c360ff8bb850fdd7b
Sathwik Sadashay V <sathwiksv30@gmail.com>
2023-08-19 10:48:48 +0530

    first commit-problem2 start

--- problem1.js
+++ problem1.js
@@ -1,3 +1,5 @@
+// Write a JavaScript function that takes an array of numbers and returns the sum of all positive numbers in the array.
+
 const array = [1, 8, -9, 3, 5, -4, 0, 1, 10, 10, 10, -5, 5, 5];
 
 
